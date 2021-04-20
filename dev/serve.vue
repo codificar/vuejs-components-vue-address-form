27c71ef411760fea32d130753b1591915efed6bd
Gustavo Silva <gustavo.silva@codificar.com.br>
2021-04-20 14:53:53 -0300

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueAddressForm from '@/vue-address-form.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueAddressForm
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-address-form />
+  </div>
+</template>
